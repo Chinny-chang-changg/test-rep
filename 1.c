ca74c7d810ea54d411292c61c1c2a8a3229677bf
Chinny-chang-changg <67921262+Chinny-chang-changg@users.noreply.github.com>
2021-07-21 13:05:39 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    //char string[100];
+    char *s[10];
+    printf("Enter A String: ");
+    scanf("%s", s);
+    printf("%s\n", s);
+    return 0;
+}
